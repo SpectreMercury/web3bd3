270a46cc3606a3f3f53e5c19df4f3076e083af3b
lee920217 <francislee920217@gmail.com>
2023-09-15 19:10:44 +0800

    remove unused file

--- src/api/fetchProfile111.ts
+++
@@ -1,8 +0,0 @@
-import { kj, getRequest } from '@/lib/api';
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const walletAddress = req.query.addr; // 假设你从查询参数中获取 walletAddress
-    const response = await getRequest(kj, `/api/profile`, { addr: walletAddress }, req, res);
-    res.json(response);
-}
